1c1f1ed30fc62cae081ae8613ccd8dbe8f2b54d1
Appz-dk <patrick.fyhn@gmail.com>
2022-06-25 19:38:26 +0200

    Added homepage + styling of homepage

--- models/campground.js
+++ models/campground.js
@@ -13,34 +13,44 @@ imageSchema.virtual("thumbnail").get(function () {
   return this.url.replace("/upload", "/upload/w_200");
 });
 
+// CampgroundSchema options (to include virtuals in JSON.stringify)
+const opts = { toJSON: { virtuals: true } };
 // Schema = mongoose.Schema aka. Schema is now a shorthand version
-const CampgroundSchema = new Schema({
-  title: String,
-  price: Number,
-  description: String,
-  location: String,
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  reviews: [
-    {
+const CampgroundSchema = new Schema(
+  {
+    title: String,
+    price: Number,
+    description: String,
+    location: String,
+    author: {
       type: Schema.Types.ObjectId,
-      ref: "Review",
+      ref: "User",
     },
-  ],
-  images: [imageSchema],
-  geometry: {
-    type: {
-      type: String,
-      enum: ["Point"],
-      required: true,
-    },
-    coordinates: {
-      type: [Number], // Array of numbers
-      required: true,
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+    images: [imageSchema],
+    geometry: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number], // Array of numbers
+        required: true,
+      },
     },
   },
+  opts
+);
+// Campground virtual property
+CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
+  return `<strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
+  <p>${this.description.substring(0, 50)}...</p>`;
 });
 
 //## Campground delete Middleware ##\\
