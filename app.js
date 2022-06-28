7afb8860ff7a383496ff13812ce591c685f29dd3
Appz-dk <patrick.fyhn@gmail.com>
2022-06-28 22:21:47 +0200

    forgot to remove dotenv config from app.js in last commit

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-require("dotenv").config();
 // console.log(process.env.CLOUDINARY_KEY);
 
 const express = require("express");
